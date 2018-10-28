17570f8ee83b12db2c3852ca1d5dd7a62f0b8d2a
Jade Corcoran <35224466+Jadelette@users.noreply.github.com>
2018-10-28 09:07:53 +0000

    Added test for numbers containing a '5' and updated translate method to pass test
    Added test for FizzBuzz for numbers containing 3&5

---
+++ src/main/potter/Potter.java
@@ -0,0 +1,65 @@
+package potter;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Potter {
+
+
+    public static void main(String[] args) {
+
+        ArrayList<String> basket = new ArrayList<String>();
+
+        boolean running = true;
+
+        while (running == true) {
+
+            System.out.println("Would you like to add a book to your basket? (yes/no)");
+            Scanner scanner = new Scanner(System.in);
+
+            if(scanner.hasNext()) {
+                String yesNo = scanner.next();
+                System.out.println(yesNo);
+
+                switch (yesNo) {
+                    case "yes":
+                        System.out.println("which volume would you like to add?");
+                        Scanner scanner2 = new Scanner(System.in);
+                        if (scanner2.hasNextInt()) {
+                            int volume = scanner2.nextInt();
+                            if (volume <= 5 && volume >= 1) {
+                                String whichBook = "Volume " + volume;
+                                basket.add(whichBook);
+                                System.out.println("added to basket: " + whichBook);
+                            } else {
+                                System.out.println("Requested volume not recognised. Please try again.");
+                            }
+                        }
+
+                        break;
+
+                    case "no":
+                        System.out.println("Items in basket:");
+                        for (String book : basket) {
+                            System.out.println(book);}
+                            System.out.println();
+                        System.out.println("Calculating cost of basket...");
+                        running = false;
+                        break;
+                    default :
+                        System.out.println("please type 'yes' or 'no'");
+                                    }
+                                }
+                            }
+                        }
+
+
+
+
+
+
+        }
+
+
+
---
+++ src/test/potter/PotterTest.java
@@ -0,0 +1,40 @@
+package potter;
+
+import potter.Potter;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class PotterTest {
+
+    /** There are five books in a series.
+     *
+     * 1 volume cost 8EUR
+     * 2 volumes = 5% discount
+     * 3 volumes = 10% discount
+     * 4 volumes = 20% discount
+     * 5 volumes = 25% discount
+     *
+     * To receive the discount, the books must be different volumes in the series
+     * (e.g. two books of the same volume gives no discount)
+     *
+     * The programme should calculate the cheapest possible option for any given basket.
+     */
+
+
+    /* example basket to test:
+    2 x book 1
+    2 x book 2
+    2 x book 3
+    1 x book four
+    1 x book five
+     */
+
+    //Step one populate basket
+   // @Test
+    //Given
+
+    //When
+
+    //Then
+
+}
