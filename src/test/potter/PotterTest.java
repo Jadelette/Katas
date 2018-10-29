d678231e3b7e876288f2fe49ae281fb913713f75
Timothy Pharo <Tim.Pharo@autotrader.co.uk>
2018-10-29 07:18:18 +0000

    Some minor suggestions Issue #1

--- src/test/potter/PotterTest.java
+++ src/test/potter/PotterTest.java
@@ -1,12 +1,9 @@
 package potter;
 
-import potter.Potter;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
-
-import static org.junit.Assert.*;
 
 public class PotterTest {
 
@@ -16,16 +13,15 @@ public class PotterTest {
         Potter potter = new Potter();
 
         //When
-
-        ArrayList<String> basket = new ArrayList<String>();
-        basket.add(0, "Volume 1");
-        basket.add(1, "Volume 1");
-        basket.add(2, "Volume 2");
-        basket.add(3, "Volume 2");
-        basket.add(4, "Volume 3");
-        basket.add(5, "Volume 3");
-        basket.add(6, "Volume 4");
-        basket.add(7, "Volume 4");
+        ArrayList<String> basket = new ArrayList<>();
+        basket.add("Volume 1");
+        basket.add("Volume 1");
+        basket.add("Volume 2");
+        basket.add("Volume 2");
+        basket.add("Volume 3");
+        basket.add("Volume 3");
+        basket.add("Volume 4");
+        basket.add("Volume 4");
 
         int result = potter.calculateBasketTotal(basket);
 
@@ -33,22 +29,21 @@ public class PotterTest {
         Assert.assertEquals(64, result);
     }
 
-    @test
+    @Test
     public void testsIfVolumesAreDistinguishedAndGrouped(){
         //Given
         Potter potter = new Potter();
 
         //When
-
-        ArrayList<String> basket = new ArrayList<String>();
-        basket.add(0, "Volume 1");
-        basket.add(1, "Volume 1");
-        basket.add(2, "Volume 2");
-        basket.add(3, "Volume 2");
-        basket.add(4, "Volume 3");
-        basket.add(5, "Volume 3");
-        basket.add(6, "Volume 4");
-        basket.add(7, "Volume 4");
+        ArrayList<String> basket = new ArrayList<>();
+        basket.add("Volume 1");
+        basket.add("Volume 1");
+        basket.add("Volume 2");
+        basket.add("Volume 2");
+        basket.add("Volume 3");
+        basket.add("Volume 3");
+        basket.add("Volume 4");
+        basket.add("Volume 4");
 
         int result = potter.calculateBasketTotal(basket);
 
