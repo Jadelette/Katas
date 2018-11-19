031f75d8c733d5404bdd4c7893d9c188c946e7e3
Jade Corcoran <35224466+Jadelette@users.noreply.github.com>
2018-11-19 19:19:49 +0000

    Bowling - several tests and methods added Getting there

--- src/main/bowling/Bowling.java
+++ src/main/bowling/Bowling.java
@@ -2,10 +2,35 @@ package bowling;
 
 public class Bowling {
 
-    //return 0 when no rolls
+    public static void main(String[] args) {
 
-    public int getTotalScore() {
+            }
+
+    public static int calculateSpare(int nextRollAfterSpare) {
+        int spareFrameTotal = 10 + nextRollAfterSpare;
+        return spareFrameTotal;
+    }
+
+    public int getFrameScore(int rollOne, int rollTwo) {
+
+        int frameTotal = rollOne + rollTwo;
+
+        if (frameTotal==10) {
+            frameTotal = Bowling.calculateSpare(5); //need to create method to determine next roll score
+        }
+        return frameTotal;
+    }//end of getFrameScore
+
+    public int getTotalScore(int... gameScores) {
+
         int totalScore = 0;
+
+        for (int frameTotal : gameScores) {
+                totalScore += frameTotal;
+        }
+
         return totalScore;
     } // end of get total score method
+
+
 } //end of class
--- src/test/bowling/BowlingTest.java
+++ src/test/bowling/BowlingTest.java
@@ -18,8 +18,66 @@ public class BowlingTest {
 
     }//end of test
 
+   @Test
+    public void addsScoreToTotalForOneFrame() {
+        //Given
+        Bowling game1 = new Bowling();
+
+        //When
+        int result = game1.getTotalScore(3);
+
+        //Then
+        Assert.assertEquals(3, result);
+    } //end of second test
+
+    @Test
+    public void addsScoreForMultipleFrames() {
+        //Given
+        Bowling game1 = new Bowling();
+
+        //When
+        int result = game1.getTotalScore(3, 5, 7, 9);
+
+        //Then
+        Assert.assertEquals(24, result);
+    } //end of test 3
+
+    @Test
+    public void returnsTotalForASingleFrame() {
+        //Given
+        Bowling game1 = new Bowling();
+
+        //When
+        int result = game1.getFrameScore(3,5);
+
+        //Then
+        Assert.assertEquals(8, result);
+    } //end of test 4
 
 
+    @Test
+    public void canCalculateSpare() {
+        //Given
+        Bowling game1 = new Bowling();
+
+        //When
+        int result = game1.calculateSpare(5);
+
+        //Then
+        Assert.assertEquals(15, result);
+    } //end of test 5
+
+    @Test
+    public void canDetectSpareAndClaculateTotalIfNextRollIsFive() {
+        //Given
+        Bowling game1 = new Bowling();
+
+        //When
+        int result = game1.getFrameScore(1,9);
+
+        //Then
+        Assert.assertEquals(15, result);
+    }
 
 
 
